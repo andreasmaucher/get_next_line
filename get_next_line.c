41b23e61168939808288d9fe613cf37a26aa1595
Andreas Maucher <Andreas-maucher@gmx.de>
2022-12-30 18:11:39 +0100

    added_functions

--- get_next_line.c
+++ get_next_line.c
@@ -71,34 +71,35 @@ char	*ft_line(char *buf)
 
 char    *ft_read_fromfile(int fd, char *linetempstore)
 {
-char    *buf;
-//int     count;
-int     byte_read;
+        char    *buf;
+        //int     count;
+        int     byte_read;
+        int     BUFFER_SIZE = 30;
 
-// malloc if res dont exist
-	if (!linetempstore)
-		linetempstore = ft_calloc(1, 1);
-	// malloc buffer
-	buf = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
-// count can be substituted with buf_size later, this is just for easier testing
-//count = 10; 
-byte_read = 1;
-while (byte_read > 0) // not sure yet, why we need this loop
-{
-        // read returns number of byte reads by default (zero indicates end of file)
-        byte_read = read(fd, buf, BUFFER_SIZE);
-        // read returns -1 on error
-                if (byte_read == -1)
-	{
-	        free(buf);
-		return (NULL);
-	}
-         // ensure that the buffer is null terminated
-        buf[byte_read] = '\0';
-        linetempstore = ft_free_tempstore(linetempstore, buf);
-// strchr will print everything of a string that comes after (incl.) the searchable character if it is found
-// if the character is not found withing a string it returns null
-// so we say here if we have read until end of line, please stop the while loop! this is our loop terminator
+        //count = 10; 
+        // malloc if res dont exist
+                if (!linetempstore)
+                        linetempstore = ft_calloc(1, 1);
+                // malloc buffer
+                buf = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
+        // count can be substituted with buf_size later, this is just for easier testing
+        byte_read = 1;
+        while (byte_read > 0) // not sure yet, why we need this loop
+        {
+                // read returns number of byte reads by default (zero indicates end of file)
+                byte_read = read(fd, buf, BUFFER_SIZE);
+                // read returns -1 on error
+                        if (byte_read == -1)
+                {
+                        free(buf);
+                        return (NULL);
+                }
+                // ensure that the buffer is null terminated
+                buf[byte_read] = '\0';
+                linetempstore = ft_free_tempstore(linetempstore, buf);
+        // strchr will print everything of a string that comes after (incl.) the searchable character if it is found
+        // if the character is not found withing a string it returns null
+        // so we say here if we have read until end of line, please stop the while loop! this is our loop terminator
         if (ft_strchr(buf, '\n'))
 	        break ;
 }
@@ -110,11 +111,33 @@ char	*get_next_line(int fd)
 {
         static char    *buf;
         char    *line;
+        int     BUFFER_SIZE = 30;
 
+        if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
+		return (NULL);
         buf = ft_read_fromfile(fd, buf);
+        if (!buf)
+		return (NULL);
         line = ft_line(buf);
         buf = ft_next(buf);
         return (line);
+}
+
+int main()
+{
+  int fd;
+  int ret;
+  char *line;
+  
+  fd = open("test.txt", O_RDONLY);
+  ret = 1;
+  while (ret > 0)
+  {
+    ret = get_next_line(fd);
+    printf("ret = [%i]\t[%s]", ret, line);
+    //free(line);
+  }
+  return (ret);
 }
 /*
 int	main()
@@ -125,5 +148,27 @@ int ptr;
  if (ptr == 0)
       printf("file can't be opened \n");
 printf("%s",get_next_line(ptr));
+}
+
+
+// setup for testing, probably I need a while loop to see different line
+int	main()
+{
+int fd;
+static char    *buf;
+        char    *line;
+ // Opening file in reading mode to get the FD
+ fd = open("test.txt", O_RDONLY);
+ if (fd == 0)
+      printf("file can't be opened \n");
+//printf("%s",get_next_line(ptr));
+        buf = ft_read_fromfile(fd, buf);
+        printf("%s\n", buf);
+        line = ft_line(buf);
+        printf("%s\n", line);
+        buf = ft_next(buf);
+        printf("%s\n", buf);
+        //return (line);
+        printf("%s\n", line);
 }
 */
